57cb675d79dca27d6418ef55f161be32747545a6
Starman-x64 <starman.x64@gmail.com>
2024-01-10 14:27:02 +0800

    archive backup: 2024-01-10 14:27:02

--- kessokuband-rplace/_templates/scripts/parse_title.js
+++ kessokuband-rplace/_templates/scripts/parse_title.js
@@ -1,16 +1,22 @@
+function modulo(a, b) {
+  // if b is 3, then {..., 3, 2, 1, 0, -1, -2, -3, ...} => {..., 0, 2, 1, 0, 2, 1, 0, ...}
+  let A = Math.floor(a);
+  return a - b*Math.floor(A/b); 
+}
+
 function parse_title(title) {
   var dateArray = [2023, 7, 20];
   var timeArray = title.slice(0, 6).split("-").map(x => parseInt(x));
   var remainder = title.slice(6);
-  // add seven hours to time to make it utc+0
-  timeArray[0] += 7; 
+  // add correct amount of hours to time to make it utc+0
+  timeArray[0] += (new Date()).getTimezoneOffset()/60; 
   // adjust date if needed
-  if (timeArray[0] >= 24) {
-    timeArray[0] %= 24;
-    dateArray[2]++;
+  if (timeArray[0] < 0 || timeArray[0] >= 0) {
+    dateArray[2] += timeArray[0] < 0 ? -1 : 1;
+    timeArray[0] = modulo(timeArray[0], 24);
     // won't have to worry about incrementing the month or year as it ended on the 25th
   }
   remainder = remainder == "" ? "" : " " + remainder;
-  return dateArray.map(x => x >= 1000 ? x : x.toString().padStart(2, "0")).join("-") + "_" + timeArray.map(x => x.toString().padStart(2, "0")).join("-") + remainder;
+  return dateArray.map(x => x >= 1000 ? x : x.toString().padStart(2, "0")).join("-") + " " + timeArray.map(x => x.toString().padStart(2, "0")).join("-") + remainder;
 }
 module.exports = parse_title;
